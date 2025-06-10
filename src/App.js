f7d3aa1272e8aa2276479b20ce9177da28ae20e9
lramir14 <73917621+lramir14@users.noreply.github.com>
2025-06-10 21:26:51 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import NavigationHeader from './components/NavigationHeader';
-import HeroSection from './components/HeroSection';
-import AboutSection from './components/AboutSection';
-import ProjectsSection from './components/ProjectsSection';
-import ContactSection from './components/ContactSection';
-import Footer from './components/Footer';
+import { NavigationHeader } from './components/NavigationHeader';
+import { HeroSection } from './components/HeroSection';
+import { AboutSection } from './components/AboutSection';
+import { ProjectsSection } from './components/ProjectsSection';
+import { ContactSection } from './components/ContactSection';
+import { Footer } from './components/Footer';
 
 function App() {
     return (
