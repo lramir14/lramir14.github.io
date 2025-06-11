79b85e039457bfe0b42ae736ceb2ad146d002f9f
lramir14 <73917621+lramir14@users.noreply.github.com>
2025-06-11 19:46:44 +0200

    more debugging

--- main.tsx
+++ main.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import App from './App.tsx'; // Or import Home from './ui/home.tsx';
+import Home from './ui/home.tsx'; // Or import Home from './ui/home.tsx';
 import '/ui/src/index.css'; // Your global CSS
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
