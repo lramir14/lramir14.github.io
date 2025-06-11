3f21d8cda0de4e3b300fdea4dae98b1442b6e82b
lramir14 <73917621+lramir14@users.noreply.github.com>
2025-06-11 18:19:26 +0200

    updates

--- ui/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './home' // Now matches the filename and component name
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <Home />
-    </React.StrictMode>
-)
