fc777e35c700eb7c726063dd03e005c701485ad9
lramir14 <73917621+lramir14@users.noreply.github.com>
2025-06-11 18:47:18 +0200

    debugs

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './home' // No extension needed
+import Home from './home.tsx' // No extension needed
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
