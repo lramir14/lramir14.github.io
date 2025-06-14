1f016b1cdf6f581253e3325f9ba2a539cbb00bef
lramir14 <73917621+lramir14@users.noreply.github.com>
2025-06-14 19:22:40 +0200

    almost ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // This is the correct setting for your lramir14.github.io User Page
   plugins: [react()],
 })
